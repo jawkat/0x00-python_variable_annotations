33630be4beb30fa3aab55f2e8cccda621ceed990
jawkat <kad.joliano@gmail.com>
2024-08-01 15:24:53 +0100

    task 2

---
+++ 0x00-python_variable_annotations/2-floor.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+from math import floor as fl
+
+
+def floor(n: float) -> int:
+    """_summary_
+
+    Args:
+        n (float): _description_
+
+    Returns:
+        float: _description_
+    """
+    return fl(n)
