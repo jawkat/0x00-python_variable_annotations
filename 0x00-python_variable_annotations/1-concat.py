3adc73b9385da3a07d587d11f158b9575e9cfc97
jawkat <kad.joliano@gmail.com>
2024-08-01 15:11:18 +0100

    task 2

---
+++ 0x00-python_variable_annotations/1-concat.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+""" concatenated two string """
+
+
+def concat(str1: str, str2: str) -> str:
+    """_summary_
+
+    Args:
+        str1 (str): _description_
+        str2 (str): _description_
+
+    Returns:
+        str: _description_
+    """
+    return f"{str1}{str2}"
