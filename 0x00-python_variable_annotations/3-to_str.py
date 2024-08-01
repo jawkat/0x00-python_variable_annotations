1e1455be341467088c81e28d41198d432223f3e5
jawkat <kad.joliano@gmail.com>
2024-08-01 15:29:53 +0100

    task 3

---
+++ 0x00-python_variable_annotations/3-to_str.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+""" return representation string """
+
+
+def to_str(n: float) -> str:
+    """_summary_
+
+    Args:
+        n (float): _description_
+
+    Returns:
+        str: _description_
+    """
+    return str(n)
