0dea11cc2e30c518393b14833148a539b20c7979
jawkat <kad.joliano@gmail.com>
2024-08-01 14:53:57 +0100

    task 0

---
+++ 0x00-python_variable_annotations/0-add.py
@@ -0,0 +1,12 @@
+""" function that add two float """
+def add(a:float, b:float) -> float:
+    """_summary_
+
+    Args:
+        a (float): _description_
+        b (float): _description_
+
+    Returns:
+        float: _description_
+    """
+    return a+b
