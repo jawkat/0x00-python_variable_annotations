1cf7260b3783a0b3d0f9f95b24b0f210faf42cdb
jawkat <kad.joliano@gmail.com>
2024-08-01 16:12:29 +0100

    task 7

--- 0x00-python_variable_annotations/7-to_kv.py
+++ 0x00-python_variable_annotations/7-to_kv.py
@@ -1,1 +1,16 @@
 #!/usr/bin/env python3
+""" comments first line """
+from typing import Tuple, Union
+
+
+def to_kv(k: str, v: Union[int, float]) -> Tuple[str, float]:
+    """_summary_
+
+    Args:
+        k (int): _description_
+        v (Union[int, float]): _description_
+
+    Returns:
+        tuple: _description_
+    """
+    return (k, v ** 2)
