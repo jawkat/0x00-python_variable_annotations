bbd6d260eaff7d90cf6952c66b307c8f6cbc4639
jawkat <kad.joliano@gmail.com>
2024-08-01 16:38:20 +0100

    task 101

---
+++ 0x00-python_variable_annotations/101-safely_get_value.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python3
+from typing import Any, Mapping, TypeVar, Union
+# Définir une variable de type générique T
+T = TypeVar('T')
+
+
+# Annoter les paramètres et le type de retour de la fonction
+def safely_get_value(dct: Mapping[Any, Any],
+                     key: Any,
+                     default: Union[T, None] = None) -> Union[Any, T]:
+    """
+    Récupère la valeur associée à `key` dans `dct`.
+    Si `key` n'existe pas dans `dct`,
+    retourne la valeur par défaut `default`.
+
+    :param dct: Un dictionnaire ou une autre forme de mapping
+    avec des clés et des valeurs de tout type.
+    :param key: La clé à rechercher dans le dictionnaire.
+    :param default: La valeur par défaut à retourner si la c
+    lé n'existe pas dans le dictionnaire.
+                    Peut être de n'importe quel type ou None.
+    :return: La valeur associée à la clé si elle existe,
+    sinon la valeur par défaut.
+    """
+    if key in dct:
+        return dct[key]
+    else:
+        return default
