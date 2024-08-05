12b5d2bf35271cb9cc85d7e9e4b4d54035e69129
jawkat <kad.joliano@gmail.com>
2024-08-05 16:12:24 +0100

    task 1

--- 0x01-python_async_function/1-concurrent_coroutines.py
+++ 0x01-python_async_function/1-concurrent_coroutines.py
@@ -1,1 +1,33 @@
 #!/usr/bin/env python3
+"""_summary_
+
+    Returns:
+        _type_: _description_
+"""
+import asyncio
+wait_random = __import__('0-basic_async_syntax').wait_random
+
+
+async def wait_n(n: int, max_delay: int) -> list:
+    """_summary_
+
+    Args:
+        n (int): _description_
+        max_delay (int): _description_
+
+    Returns:
+        _type_: _description_
+    """
+
+    tasks = [asyncio.create_task(wait_random(max_delay)) for _ in range(n)]
+    delays = await asyncio.gather(* tasks)
+
+    sorted_delays = []
+    while delays:
+        mini = delays[0]
+        for delay in delays:
+            if delay < mini:
+                mini = delay
+        sorted_delays.append(mini)
+        delays.remove(mini)
+    return sorted_delays
