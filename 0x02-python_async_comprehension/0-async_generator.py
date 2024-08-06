036b0526e82ea13e52b4832d1049fcb47b9734ae
jawkat <kad.joliano@gmail.com>
2024-08-06 12:49:25 +0100

    task 0

---
+++ 0x02-python_async_comprehension/0-async_generator.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+""" task 0 """
+import asyncio
+import random
+
+
+async def async_generator() -> float:
+    """_summary_
+
+    Yields:
+        _type_: _description_
+    """
+    for _ in range(10):
+        await asyncio.sleep(1)
+        yield random.uniform(0,10)
