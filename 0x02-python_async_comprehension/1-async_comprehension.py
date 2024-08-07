c3062aba1df95ae7e3d81026afd189032291257a
jawkat <kad.joliano@gmail.com>
2024-08-07 09:02:00 +0100

    fix pycode style

--- 0x02-python_async_comprehension/1-async_comprehension.py
+++ 0x02-python_async_comprehension/1-async_comprehension.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 """_summary_ task 1"""
-from typing import Generator, List
-async_generator =__import__('0-async_generator').async_generator
+from typing import List
+async_generator = __import__('0-async_generator').async_generator
 
 
 async def async_comprehension() -> List[float]:
